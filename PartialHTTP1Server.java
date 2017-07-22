c19fff597a907814fdda5264d905edfaff515a4f
Derek Schatel <dschatel@gmail.com>
2017-07-22 14:25:27 -0400

    Adds header creation and changes file reading to byte array

--- PartialHTTP1Server.java
+++ PartialHTTP1Server.java
@@ -2,10 +2,14 @@ import java.lang.*;
 import java.net.*;
 import java.io.*;
 import java.util.concurrent.*;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class PartialHTTP1Server implements Runnable {
 
 	private Socket csocket;
+	private byte[] fileBytes;
 
 	PartialHTTP1Server (Socket csocket) {
 		this.csocket = csocket;
@@ -69,63 +73,166 @@ public class PartialHTTP1Server implements Runnable {
 		}
 	}
 
+	private String contentType(String extension) {
+
+		String contentType = "";
+
+		switch(extension) {
+
+		case "html":
+			contentType = "text/html";
+			break;
+		case "c":
+		case "c++":
+		case "cc":
+		case "com":
+		case "conf":
+		case "cxx":
+		case "def":
+		case "f":
+		case "f90":
+		case "for":
+		case "g":
+		case "h":
+		case "hh":
+		case "idc":
+		case "jav":
+		case "java":
+		case "list":
+		case "log":
+		case "lst":
+		case "m":
+		case "mar":
+		case "pl":
+		case "sdml":
+		case "text":
+		case "txt":
+			contentType = "text/plain";
+			break;
+		case "gif":
+			contentType = "image/gif";
+			break;
+		case "jfif":
+		case "jfif-tbnl":
+		case "jpe":
+		case "jpeg":
+		case "jpg":
+			contentType = "image/jpeg";
+			break;
+		case "png":
+		case "x-png":
+			contentType = "image/png";
+			break;
+		case "pdf":
+			contentType = "application/pdf";
+			break;
+		case "gz":
+		case "gzip":
+			contentType = "application/x-gzip";
+			break;
+		case "zip":
+			contentType = "application/zip";
+			break;
+		default:
+			contentType = "application/octet-stream";
+			break;
+
+		}
+
+		return contentType;
+
+	}
+
+	private String buildHeader (File file, String fileName) {
+		String header = "";
+
+		String extension = "";
+
+		if (fileName.indexOf('.') != -1)
+			extension = fileName.substring(fileName.indexOf('.')+1);
+		header += "Content-Type: " + contentType(extension) + '\r' + '\n';
+		header += "Content-Length: " + file.length() + '\r' + '\n';
+
+		SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy kk:mm:ss zzz");
+
+		header += "Last-Modified: " + sdf.format(file.lastModified()) + '\r' + '\n';
+		header += "Content-Encoding: identity" + '\r' + '\n';
+		header += "Allow: GET, POST, HEAD" + '\r' + '\n';
+
+		Calendar now = Calendar.getInstance();
+		now.add(Calendar.HOUR, 24);
+		header += "Expires: " + sdf.format(now.getTime()) + '\r' + '\n';
+
+
+
+		return header;
+
+
+	}
+
 	private String parseClientInput(String clientInput) {
 		if (clientInput == null)
-			return "400 Bad Request";
+			return "HTTP/1.0 400 Bad Request";
 
 		String[] tokens = clientInput.split("\\s+");
 
-		//1. Parse format. Only two tokens, and first token is capitalized? Does the second token begin with /? Does the third token begin with HTTP/?
-		if(tokens.length != 3 || !tokens[0].toUpperCase().equals(tokens[0]) || tokens[1].charAt(0) != '/' || !tokens[2].substring(0,5).equals("HTTP/") || tokens[2].substring(5) == null)
-			return "400 Bad Request";
-
 		float versionNum;
+
+		//1. Parse format. Only three tokens, and first token is capitalized? Does the second token begin with /? Does the third token begin with HTTP/?
+		if(tokens.length != 3 || !tokens[0].toUpperCase().equals(tokens[0]) || tokens[1].charAt(0) != '/' || !tokens[2].substring(0,5).equals("HTTP/") || tokens[2].substring(5) == null)
+			return "HTTP/1.0 400 Bad Request";
 
 		try {
 			versionNum = Float.parseFloat(tokens[2].substring(5));
 		} catch (NumberFormatException num) {
-			return "400 Bad Request";
+			return "HTTP/1.0 400 Bad Request";
 		}
 
 		if (versionNum > 1.0 || versionNum < 0.0)
-			return "505 HTTP Version Not Supported";
+			return tokens[2] + " 505 HTTP Version Not Supported";
 
 		//2. Parse command. Is the first token GET?
 		if(!tokens[0].equals("GET"))
-			return "501 Not Implemented";
+			return tokens[2] + " 501 Not Implemented";
 
 		//3. Parse file. Does the file exist on the server?
 		String filePath = "." + tokens[1];
 
 		File f = new File(filePath);
 		if (!f.isFile())
-			return "404 Not Found";
+			return tokens[2] + "404 Not Found";
 
-		BufferedReader br = null;
-		FileReader fr = null;
+		//BufferedReader br = null;
+		//FileReader fr = null;
+		Path path = Paths.get(filePath);
 
 		//4. Otherwise, try to open the file and send response.
 		try {
 
-			fr = new FileReader(filePath);
-			br = new BufferedReader(fr);
+			//fr = new FileReader(filePath);
+			//br = new BufferedReader(fr);
+			fileBytes = Files.readAllBytes(path);
 
-			String currentLine;
-			String response = "200 OK\n\n";
 
-			while ((currentLine = br.readLine()) != null) {
-				response+=currentLine;
-			}
+			//String currentLine;
+			String response = tokens[2] + " 200 OK" + '\r' + '\n';
 
-			response+="\n";
-			br.close();
-			fr.close();
+			response += buildHeader(f, tokens[1]);
+
+
+			//while ((currentLine = br.readLine()) != null) {
+			//	response+=currentLine;
+			//}
+
+			response+='\n';
+			//br.close();
+			//fr.close();
 
 			return response;
 
 		} catch (IOException e) {
 			//5. Did anything go wrong with the request?
-			return "500 Internal Error";
+			return tokens[2] + "403 Forbidden";
 		}
 
 	}
@@ -135,21 +242,33 @@ public class PartialHTTP1Server implements Runnable {
 			//Read in input from client, parse input, return proper code
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(csocket.getInputStream()));
 			PrintWriter outToClient = new PrintWriter(csocket.getOutputStream(), true);
+			String command = "";
+			String modified = "";
+			int numLines = 0;
 
 			try {
 				//Set server socket timeout
 				//csocket.setSoTimeout(3000);
 				//Read in message from client
-				String messageFromClient = inFromClient.readLine();
-				System.out.println("Read from client: " + messageFromClient);
+				while (numLines != 2) {
+					if (numLines == 0)
+						command = inFromClient.readLine();
+					else if (numLines == 1)
+						modified = inFromClient.readLine();
+					numLines++;
+				}
+				//String messageFromClient = inFromClient.readLine();
+				//System.out.println("Read from client: " + messageFromClient);
+				System.out.println("Command: " + command + "; Modified: " + modified);
+				System.out.println("Num Lines: " + numLines);
 				//Parse input from client
-				String response = parseClientInput(messageFromClient);
+				String response = parseClientInput(command);
 				System.out.println("Sending response: " + response);
 				//Send response code to client
 				outToClient.println(response);
 			} catch (SocketTimeoutException e){
 				//If client does not send data in 3 seconds, send back timeout error
-				outToClient.println("408 Request Timeout");
+				outToClient.println("HTTP/1.0 408 Request Timeout");
 			}
 			//Close connections
 			inFromClient.close();
